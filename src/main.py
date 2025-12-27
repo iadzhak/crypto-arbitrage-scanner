1138c9c90f3fd811c0678f77bd4520b81ebece0b
Sergei Iadzhak <yadzhak@yandex.ru>
2025-12-28 00:12:39 +0300

    entrypoint improved

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
 from gui import create_app
+from scanner import Scanner, thread_run
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 if __name__ == "__main__":
-    app = create_app()
+    # Исправление предупреждения об утечке семафоров в multiprocessing
+    # Актуально только для debug-режима Flask.
+    import multiprocessing
+
+    multiprocessing.set_start_method('spawn', force=True)
+
+    # Основная точка входа.
+    scanner = Scanner()
+    thread = thread_run(scanner)
+    thread.start()
+    app = create_app(scanner)
     app.run(debug=True)
+    scanner.stop()
+    thread.join()
